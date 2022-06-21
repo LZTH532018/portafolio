d6900d70ecbe84aba45fb6358932245f35c760b7
LZTH532018 <luisgerardotf5@gmail.com>
2022-06-21 18:11:54 -0500

    Change SimpleText zinc-500 to zinc-400

--- src/components/atoms/SimpleText.jsx
+++ src/components/atoms/SimpleText.jsx
@@ -1,6 +1,6 @@
 const SimpleText = ({text}) => {
     return(
-        <div className="text-zinc-500 font-semibold 2xl:text-2xl">
+        <div className="text-zinc-400 font-semibold 2xl:text-2xl">
             <p>{text}</p>
         </div>
     )
